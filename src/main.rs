e0c30774107b01d747ec3038aa9eb27dd7445421
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-25 20:41:22 -0500

    working through types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    // mutable
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    // shadow
+    let shadow = 0b0000_1111;
+    println!("The value of shadow is: {}", shadow);
+    let shadow = 'a';
+    println!("The value of shadow is: {}", shadow);
+
+    // tuples
+    let tup = ('a', 0b00001111, 25.6);
+    let (f, s, t) = tup;
+    println!("The value of f is: {}", f);
 }

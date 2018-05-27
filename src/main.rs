444ba1e4d8931c0bef64a195bb0cab253e300827
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-26 20:26:11 -0500

    control-flow samples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let bank_account = 400;
+
+    let emotion = if bank_account < 500 {
+        "sad"
+    } else {
+        "happy"
+    };
+
+    println!("emotion is {}", emotion);
+
+    for_in();
+
+    range();
+}
+
+fn for_in() {
+    let a = [1,2,3,4,5,6];
+    for val in a.iter() {
+        println!("val is {}", val);
+    }
+}
+
+fn range() {
+    for a in (1..5).rev() {
+        println!("{}", a);
+    }
 }

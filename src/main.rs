70e769c6a6d81758093afff4be93c7ede8c1e184
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-26 20:10:55 -0500

    func expressions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    another_fn();
+    let five = five();
+    println!("{}", five);
+}
+
+fn another_fn() {
+    println!("Another fn");
+}
+
+fn five() -> i32 {
+    5
 }

40f6a3191f1342c428334e9aede8edad0c6e055d
Larry Foulkrod <lefoulkrod@gmail.com>
2018-05-30 09:50:01 -0500

    test borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s1 = String::from("hello");
+    
+    {
+        let r1 = &s1;
+        borrow_test(r1);
+    }
+
+    { 
+        let r2 = &mut s1;
+        mutate(r2);
+    }
+    println!("mutated {}", s1);
+}
+
+fn borrow_test(s1: &String) {
+    println!("borrowed {}", s1)
+}
+
+fn mutate(s1: &mut String) {
+    s1.push_str(", world");
 }

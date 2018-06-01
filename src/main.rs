f4c8837306d20bf59c4560fc9e7889db3b602ddf
Larry Foulkrod <lefoulkrod@gmail.com>
2018-06-01 15:34:42 -0500

    slices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    send_slice(hello);
+}
+
+fn send_slice(slice: &str) {
+    println!("{}", slice);
 }
